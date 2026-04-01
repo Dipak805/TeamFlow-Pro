1469f023c1d4c5e7e8734dd0834d03a6fb0b7fe7
Dipak805 <gundd526@gmail.com>
2026-04-01 11:27:54 +0530

    Configure Render Deployment

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const baseURL = process.env.REACT_APP_API_URL 
+  ? `${process.env.REACT_APP_API_URL}/api` 
+  : 'http://localhost:8080/api';
+
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL,
   headers: { 'Content-Type': 'application/json' },
 });
 
